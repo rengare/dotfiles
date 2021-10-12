fdbccf1ff116b571f531436e9bb24383ab657031
Gracjan Gorecki <gorecki.gracjan@hotmail.com>
2021-10-12 22:44:49 +0200

    changes in lvim config

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -154,6 +154,9 @@ lvim.plugins = {
   {
     'jiangmiao/auto-pairs'
   },
+  {
+    "jose-elias-alvarez/nvim-lsp-ts-utils",
+  },
   {"sainnhe/gruvbox-material"},
   {"tpope/vim-commentary"},
   {
@@ -178,4 +181,50 @@ lvim.plugins = {
     end
   }
 }
+
+lvim.lsp.on_attach_callback = function(client, _)
+  if client.name == "tsserver" then
+
+    client.resolved_capabilities.document_formatting = false
+    client.resolved_capabilities.document_range_formatting = false
+    
+    require"nvim-lsp-ts-utils".setup{
+            debug = true,
+            disable_commands = false,
+            enable_import_on_completion = true,
+            -- import all
+            import_all_timeout = 5000, -- ms
+            import_all_priorities = {
+                buffers = 4, -- loaded buffer names
+                buffer_content = 3, -- loaded buffer content
+                local_files = 2, -- git files or files with relative path markers
+                same_file = 1, -- add to existing import statement
+            },
+            import_all_scan_buffers = 100,
+            import_all_select_source = false,
+
+            -- eslint
+            eslint_enable_code_actions = true,
+            eslint_enable_disable_comments = true,
+            eslint_bin = "eslint",
+            eslint_enable_diagnostics = false,
+            eslint_opts = {},
+
+            -- formatting
+            enable_formatting = false,
+            formatter = "prettier",
+            formatter_opts = {},
+
+            -- update imports on file move
+            update_imports_on_move = false,
+            require_confirmation_on_move = false,
+            watch_dir = nil,
+
+            -- filter diagnostics
+            filter_out_diagnostics_by_severity = {},
+            filter_out_diagnostics_by_code = {},
+    }
+    require"nvim-lsp-ts-utils".setup_client(client)
+  end
+end
 

ca891d48b8eca4430aae476cea311c79ef9d6e83
Gracjan Gorecki <gorecki.gracjan@hotmail.com>
2022-02-08 11:30:01 +0100

    changes

--- .config/i3/scripts/autostart.sh
+++ .config/i3/scripts/autostart.sh
@@ -3,12 +3,12 @@ sleep 1
 nitrogen --restore &
 #systemctl --user restart spotifyd.service &
 insync start &
-blueberry-tray &
+blueman-applet &
 # nm-applet --no-agent --indicator &
 /usr/bin/spice-vdagent &
-pactl unload-module module-raop-discover &
-pactl load-module module-raop-discover &
+pactl unload-module module-raop-discover
+pactl load-module module-raop-discover
 bluetoothctl connect 50:97:1C:5F:44:D2 &
-bluetoothctl connect 64:0B:D7:1A:86:90 &
+bluetoothctl connect D7:6A:25:A6:07:04 &
 
-powerprofilesctl set power-saver &
+# powerprofilesctl set power-saver &

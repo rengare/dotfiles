eea49da4b44575b79f7342dd57654feafbbb5bf9
Gracjan Gorecki <gorecki.gracjan@hotmail.com>
2022-02-17 12:25:01 +0100

    changes

--- .config/i3/scripts/always.sh
+++ .config/i3/scripts/always.sh
@@ -1,15 +1,11 @@
-# bluetoothctl power on
 autotiling &
-# ~/.cargo/bin/bsptile &
-~/.config/i3/scripts/switch_randr.sh
 
 if [[ "$XDG_SESSION_TYPE" =~ "x11" ]]; then
-    nitrogen --restore
+    # ~/.config/polybar/launch.sh &
+    ~/.config/i3/scripts/switch_randr.sh &
+    ~/.config/i3/scripts/picom &
+    nitrogen --restore &
     xset s off -dpms &
-    bash ~/.config/i3/scripts/picom &
 fi
 
 gammastep -P -O 4500 &
-
-# bluetoothctl connect 50:97:1C:5F:44:D2
-# bluetoothctl connect D7:6A:25:A6:07:04 &
--- .config/i3/scripts/autostart.sh
+++ .config/i3/scripts/autostart.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-dunst --config ~/.config/dunst/dunstrc &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+sleep 1
+dunst --config ~/.config/dunst/dunstrc &
 nitrogen --restore &
 #systemctl --user restart spotifyd.service &
 insync start &
-blueman-applet &
 # nm-applet --no-agent --indicator &
 /usr/bin/spice-vdagent &
 # pactl unload-module module-raop-discover

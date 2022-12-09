c540f96d9722ffd70d8ff79503098886bdba50cb
ggorecki <gorecki.gracjan@gmail.com>
2022-12-09 12:34:17 +0100

    added macos rice

--- .config/sketchybar/items/calendar.sh
+++ .config/sketchybar/items/calendar.sh
@@ -5,7 +5,7 @@ sketchybar --add item     calendar center              \
                           icon.color=$BLACK            \
                           icon.font="$FONT:Black:12.0" \
                           icon.padding_left=15         \
-                          label.width=220               \
+                          label.width=230               \
                           label.align=right            \
                           label.color=$BLACK           \
                           label.padding_right=15       \
--- .config/sketchybar/plugins/time.sh
+++ .config/sketchybar/plugins/time.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sketchybar -m --set $NAME label="$HOME/.config/scripts/weather.sh , $(date '+%d.%m.%Y %H:%M')"
+sketchybar -m --set $NAME label="$($HOME/.config/scripts/weather.sh) , $(date '+%d.%m.%Y %H:%M')"

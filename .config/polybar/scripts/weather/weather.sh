5f5569ef7146a3e65a9fd83f236e1a9e36e0e6e1
ggorecki <gorecki.gracjan@gmail.com>
2022-12-09 12:31:22 +0100

    changes

--- .config/polybar/scripts/weather/weather.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#
-city=wrocław
-
-curl -s "https://api.openweathermap.org/data/2.5/weather?q=$city&units=metric&appid=970606528befaa317698cc75083db8b2" | grep -oP '"temp":\K[^,]*' | awk '{printf("%dC", $1)}'
--- .config/sketchybar/plugins/time.sh
+++ .config/sketchybar/plugins/time.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-city=wrocław
-appid=970606528befaa317698cc75083db8b2
-temp=$(curl -s "https://api.openweathermap.org/data/2.5/weather?q=$city&units=metric&appid=$appid" | jq .main.temp)
-
-sketchybar -m --set $NAME label="$city: $temp C , $(date '+%d.%m.%Y %H:%M')"
+sketchybar -m --set $NAME label="$HOME/.config/scripts/weather.sh , $(date '+%d.%m.%Y %H:%M')"

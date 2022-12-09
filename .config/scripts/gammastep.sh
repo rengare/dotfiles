3a6190d2813bf6c8a3c2e02f7ef4ab2ecdd73618
ggorecki <gorecki.gracjan@gmail.com>
2022-12-09 10:57:06 +0100

    changes

--- .config/scripts/gammastep.sh
+++ .config/scripts/gammastep.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/bash
 
+stateFile=~/.config/scripts/gammastep_state
+
 function off(){
     gammastep -P -O 5000 &
     killall gammastep 1>/dev/null 2>/dev/null
 		# kill -9 $(pgrep -x "gammastep");
+    echo off > $stateFile 
 }
 
 function on(){
     off
-    echo "on"
 		exec gammastep -P -O 3600 &
+    echo on > $stateFile 
 }
 
 if [[ $1 = "on" ]]; then
@@ -28,7 +31,9 @@ if [[ $1 = "toggle" ]]; then
 	fi
 fi
 
-if pgrep -x "gammastep" > /dev/null; then
+state=$(cat $stateFile)
+
+if [[ $state = "off" ]]; then
 	echo ""
 else
 	echo ""

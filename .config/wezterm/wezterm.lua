635fa9e5aa8bf449ea82fae501f2b22f8441b6cf
ggorecki <gorecki.gracjan@gmail.com>
2023-02-18 13:13:22 +0100

    changes

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require("wezterm")
+
+return {
+	font = wezterm.font("JetBrains Mono"),
+	font_size = 14.0,
+	color_scheme = "Catppuccin Mocha",
+	window_background_opacity = 0.9,
+	default_prog = { "fish" },
+}

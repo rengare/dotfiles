aca179d5954295d1eca7bbb011c042aabc682dfe
ggorecki <gorecki.gracjan@gmail.com>
2023-03-04 13:01:07 +0100

    nix

--- nix/manager.sh
+++ nix/manager.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 # check args if linux else if darwin
 
+command="home-manager switch  -b backup --extra-experimental-features nix-command --extra-experimental-features flakes --flake .#ren-$1"
+
 if [ "$1" == "linux" ]; then
-  home-manager switch --flake .#ren-linux -b backup --extra-experimental-features nix-command flakes
+    echo "linux"
+    $command
 elif [ "$1" == "darwin" ]; then
-  home-manager switch --flake .#ren-darwin -b backup --extra-experimental-features nix-command flakes
+    echo "darwin"
+    $command
 else
     echo "no args"
     exit 1

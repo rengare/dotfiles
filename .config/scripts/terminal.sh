94001a818625c93df576ce3466338bd82fea8c05
ggorecki <gorecki.gracjan@gmail.com>
2023-04-04 18:19:05 +0200

    small refactor

--- .config/scripts/terminal.sh
+++ .config/scripts/terminal.sh
@@ -1,2 +1,2 @@
 #kitty
-wezterm
+wezterm start -- fish
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,6 +5,5 @@ return {
 	font_size = 14.0,
 	color_scheme = "Catppuccin Mocha",
 	window_background_opacity = 0.9,
-	default_prog = { "fish" },
 	window_decorations = "NONE",
 }

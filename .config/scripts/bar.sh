9d31163093e6be4cd1a7eea18504ec5772b7eba4
ggorecki <gorecki.gracjan@gmail.com>
2024-05-16 10:58:27 +0200

    changes

--- .config/scripts/bar.sh
+++ .config/scripts/bar.sh
@@ -9,9 +9,28 @@ if test -f "/home/ren/.i3"; then
   fourth=$color4
 
   if type "xrandr"; then
+    # Check if monitors are duplicated
+    primary_monitor=$(xrandr --query | grep " connected primary" | cut -d" " -f1)
+    duplicated=false
+
     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-      MONITOR=$m polybar --reload main&
+      if [ "$m" != "$primary_monitor" ]; then
+        mode_primary=$(xrandr --query | grep "^$primary_monitor" -A1 | tail -n1 | cut -d" " -f4)
+        mode_secondary=$(xrandr --query | grep "^$m" -A1 | tail -n1 | cut -d" " -f4)
+        if [ "$mode_primary" == "$mode_secondary" ]; then
+          duplicated=true
+          break
+        fi
+      fi
     done
+
+    if [ "$duplicated" = true ]; then
+      MONITOR=$primary_monitor polybar --reload main&
+    else
+      for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload main&
+      done
+    fi
   else
     polybar --reload main&
   fi

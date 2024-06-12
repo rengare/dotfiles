49f02c5838c40b068faa8c5a9ede1b27d0fe630e
ggorecki <gorecki.gracjan@gmail.com>
2024-06-12 14:59:04 +0200

    changes

---
+++ .config/nvim/lua/plugins/twilight.lua
@@ -0,0 +1,11 @@
+return {
+  -- Lua
+  {
+    "folke/twilight.nvim",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
+}

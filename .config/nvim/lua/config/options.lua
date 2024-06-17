cc3bb32b465b80f6a74fb7722b34ba242424cd1f
ggorecki <gorecki.gracjan@gmail.com>
2024-06-17 14:30:39 +0200

    changes

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,3 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.wrap = true
+vim.opt.clipboard = ""
+
+vim.keymap.set("n", "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>o", [["+p]])
+vim.keymap.set("n", "<leader>d", [["+d]])

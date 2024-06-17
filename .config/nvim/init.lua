1672625a86a874650513dd0869a11ec53e8ba5ba
ggorecki <gorecki.gracjan@gmail.com>
2024-06-17 12:31:31 +0200

    changes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,19 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+local dap = require("dap")
+
+dap.configurations.rust = {
+  {
+    name = "Launch",
+    type = "codelldb",
+    request = "launch",
+    program = function()
+      return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+    end,
+    cwd = "${workspaceFolder}",
+    stopOnEntry = false,
+    args = {},
+    runInTerminal = false,
+  },
+}

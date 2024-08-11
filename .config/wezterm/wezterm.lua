a4d214f9c29e30223f75f2f887c52729dd6fa31a
ggorecki <gorecki.gracjan@gmail.com>
2024-08-11 20:04:19 +0200

    wezterm updates

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,9 +1,11 @@
 local wezterm = require("wezterm")
 
 return {
+	enable_tab_bar = false,
+	default_prog = { "fish" },
 	font = wezterm.font("JetBrains Mono Nerd Font"),
-	font_size = 14.0,
-	color_scheme = "Catppuccin Mocha",
+	font_size = 16.0,
+	color_scheme = "GruvboxDark",
 	-- window_background_opacity = 0.9,
 	hide_tab_bar_if_only_one_tab = true,
 }

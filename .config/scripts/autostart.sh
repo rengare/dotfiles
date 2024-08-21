4639b3aaa25fa210c4de341e5e243731c8ef1488
ggorecki <gorecki.gracjan@gmail.com>
2024-08-21 16:43:31 +0200

    changes

--- .config/scripts/autostart.sh
+++ .config/scripts/autostart.sh
@@ -7,44 +7,16 @@ if test -f "/home/ren/.sway"; then
   echo "sway"
 fi
 
-if [ "$DESKTOP_SESSION" == "hyprland" ]; then
-  echo "hyprland"
-fi
-
 if test -f "/home/ren/.i3"; then
   /usr/lib/xdg-desktop-portal-gtk &
-  /usr/bin/gnome-keyring-daemon --start --components=secrets &
-  /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+  /usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets &
+  /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
   xhost +si:localuser:ren
+  dunst --config ~/.config/dunst/dunstrc &
+  nm-applet --no-agent --indicator &
   echo "i3"
- fi
-
-
-if test barrier; then
-  barrier &
 fi
 
-dunst --config ~/.config/dunst/dunstrc &
-#xss-lock -- /home/ren/.config/i3/scripts/blur-lock.sh &
+bash /home/ren/.startup
 
-# cpupower frequency-set -u 1400 & 
-nm-applet --no-agent --indicator &
-xfce4-power-manager &
-/usr/bin/spice-vdagent &
-# pactl unload-module module-raop-discover
-# pactl load-module module-raop-discover
-#
 sleep 1
-# flatpak run md.obsidian.Obsidian &
-# nohup easyeffects --gapplication-service &
-nohup flatpak run com.github.wwmm.easyeffects --gapplication-service &
-
-flatpak run com.nextcloud.desktopclient.nextcloud &
-# nextcloud &
-syncthing &
-appimagelauncher.AppImage appimagelauncherd &
-
-xset -b &
-rmmod pcspkr &
-
-flatpak run dev.lizardbyte.app.Sunshine &
--- .config/scripts/gammastep.sh
+++ .config/scripts/gammastep.sh
@@ -1,35 +1,35 @@
 #!/usr/bin/bash
 
-low=3400
-high=3400
+low=5400
+high=5400
 
 stateFile=~/.config/scripts/gammastep_state
 
-function off(){
+function off() {
   if test -f "/home/ren/.sway"; then
     killall wlsunset 1>/dev/null 2>/dev/null
   fi
 
   if test -f "/home/ren/.i3"; then
-      gammastep -P -O $high &
-      killall gammastep 1>/dev/null 2>/dev/null
-      # kill -9 $(pgrep -x "gammastep");
+    gammastep -P -O $high &
+    killall gammastep 1>/dev/null 2>/dev/null
+    # kill -9 $(pgrep -x "gammastep");
   fi
-  echo off > $stateFile
+  echo off >$stateFile
 }
 
-function on(){
-    off
+function on() {
+  off
 
-    if test -f "/home/ren/.sway"; then
-      exec wlsunset -T $high &
-    fi
+  if test -f "/home/ren/.sway"; then
+    exec wlsunset -T $high &
+  fi
 
-    if test -f "/home/ren/.i3"; then
-      exec gammastep -P -O $low &
-    fi
+  if test -f "/home/ren/.i3"; then
+    exec gammastep -P -O $low &
+  fi
 
-    echo on > $stateFile 
+  echo on >$stateFile
 }
 
 if [[ $1 = "on" ]]; then
@@ -43,7 +43,7 @@ fi
 if [[ $1 = "toggle" ]]; then
 
   if test -f "/home/ren/.sway"; then
-    if pgrep -x "wlsunset" > /dev/null; then
+    if pgrep -x "wlsunset" >/dev/null; then
       off
     else
       on
@@ -51,7 +51,7 @@ if [[ $1 = "toggle" ]]; then
   fi
 
   if test -f "/home/ren/.i3"; then
-    if pgrep -x "gammastep" > /dev/null; then
+    if pgrep -x "gammastep" >/dev/null; then
       off
     else
       on
@@ -61,9 +61,8 @@ fi
 
 state=$(cat $stateFile)
 
-
 if [[ $state = "off" ]]; then
-	echo ""
+  echo ""
 else
-	echo ""
+  echo ""
 fi

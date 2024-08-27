c5deb787b49d26f166f7d9ebb45362974d561baf
ggorecki <gorecki.gracjan@gmail.com>
2024-08-27 18:32:27 +0200

    changes

--- .config/scripts/autostart.sh
+++ .config/scripts/autostart.sh
@@ -8,9 +8,9 @@ if test -f "/home/ren/.sway"; then
 fi
 
 if test -f "/home/ren/.i3"; then
-  /usr/libexec/xdg-desktop-portal-gtk &
-  /usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets &
-  /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
+  exec --no-startup-id /usr/libexec/xdg-desktop-portal-gtk &
+  exec --no-startup-id /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
+  exec --no-startup-id /usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh,gpg &
   xhost +si:localuser:ren
   dunst --config ~/.config/dunst/dunstrc &
   nm-applet --no-agent --indicator &

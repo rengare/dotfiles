562041e4cad46b646f7bf56469f3f69ff562b71b
ggorecki <gorecki.gracjan@gmail.com>
2024-08-31 13:25:55 +0200

    changes

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,12 +3,3 @@
 -- Add any additional options here
 vim.opt.wrap = true
 vim.opt.clipboard = ""
-
-vim.keymap.set("v", "<leader>y", [["+y]])
-vim.keymap.set("v", "<leader>Y", [["+Y]])
-
-vim.keymap.set("n", "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
-vim.keymap.set("n", "<leader>o", [["+p]])
-vim.keymap.set("n", "<leader>O", [["+P]])
-vim.keymap.set("n", "<leader>d", [["+d]])

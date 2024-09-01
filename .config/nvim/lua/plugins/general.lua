649f362e4b963a0aa8c31c4f2567033fbafb4682
ggorecki <gorecki.gracjan@gmail.com>
2024-09-01 16:19:19 +0200

    changes

--- .config/nvim/lua/plugins/general.lua
+++ .config/nvim/lua/plugins/general.lua
@@ -14,4 +14,10 @@ return {
       require("dap.ext.vscode").load_launchjs(nil, { codelldb = { "c", "cpp" } })
     end,
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    ft = "markdown",
+    -- build = "cd app && yarn install",
+    build = ":call mkdp#util#install()",
+  },
 }

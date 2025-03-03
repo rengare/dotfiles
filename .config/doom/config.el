96f87c8590b1484947b5b86c21316a37ce3d7971
ggorecki <gracjan.gorecki-ext@seba.swiss>
2025-03-03 15:20:57 +0100

    treesit auto

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -74,3 +74,8 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(use-package treesit-auto
+  :config
+  (global-treesit-auto-mode))
+

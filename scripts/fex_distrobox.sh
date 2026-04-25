82d2acaf13a37b3bb983153df24769dcdb9d0de2
Gracjan Górecki <gracjan.gorecki@proton.me>
2026-04-25 18:27:33 +0200

    changes

--- scripts/fex_distrobox.sh
+++ scripts/fex_distrobox.sh
@@ -6,9 +6,16 @@ rm -rf ~/.config/.fex-emu
 rm -rf ~/.local/share/.fex-emu
 rm -rf ~/.fex-emu
 
-distrobox create --image ubuntu:24.04 --root --init --additional-packages "ubuntu-desktop"
+distrobox create \
+  --name fex-steam \
+  --image ubuntu:24.04 \
+  --root \
+  --init \
+  --additional-packages "systemd libpam-systemd"
 
 #inside container
+#sudo apt update
+#sudo apt install -y python3-packaging python3-setuptools
 #sudo systemctl unmask systemd-binfmt
 #curl --silent https://raw.githubusercontent.com/FEX-Emu/FEX/main/Scripts/InstallFEX.py | python3
 #wget https://repo.steampowered.com/steam/archive/stable/steam-launcher_latest_all.deb
